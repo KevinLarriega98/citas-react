0908692579dce9d494c704f53548f1cd3c43cc79
KevinLarriega98 <kevinlarriega@gmail.com>
2024-01-16 17:55:30 +0100

    States created in form

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,7 +1,12 @@
+import Paciente from "./Paciente"
+
 const ListadoPacientes = () => {
   return (
-    <div className="w-1/2 lg:w-3/5">
-      <h2>Listado Pacientes</h2>
+    <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
+      <h2 className="font-black text-3xl text-center">Listado de pacientes</h2>
+      <p className="text-lg mt-5 text-center mb-10">Aministra tus <span className="text-indigo-600 font-bold">pacientes y citas</span></p>
+
+      <Paciente />      
     </div>
   )
 }
---
+++ src/components/Paciente.jsx
@@ -0,0 +1,17 @@
+const Paciente = () => {
+    return (
+        <div className="mx-5 my-10 bg-white shadow-md rounded-lg py-10 px-5">
+            <p className="font-bold mb-3 text-gray-700 uppercase">Mascota: <span className="font-normal normal-case">Hook</span></p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">Propietario: <span className="font-normal normal-case">Hook</span></p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">E-mail: <span className="font-normal normal-case">Hook</span></p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">Alta: <span className="font-normal normal-case">Hook</span></p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">Síntomas: <span className="font-normal normal-case">Hook</span></p>
+        </div>
+    )
+}
+
+export default Paciente
